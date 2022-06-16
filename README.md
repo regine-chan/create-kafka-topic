9ac810b0c7ebf26abccd0553ad19d9405c897b67
Regine Giskegjerde Urtegård <regine.urtegard@mercell.com>
2022-06-16 09:36:13 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,73 @@
+# Create kafka topic javascript action
+
+This action creates a kafka topic in the desired kafka cluster, authenticating with client certificate.
+
+## Inputs
+
+## `KAFKA_BROKER_URL`
+
+**Required** URL to the kafka broker.
+
+## `KAFKA_SSL_CA`
+
+**Required** Kafka certificate authority.
+
+## `KAFKA_SSL_KEY`
+
+**Required** Private certificate key.
+
+## `KAFKA_SSL_CERT`
+
+**Required** Client certificate.
+
+## `TOPIC`
+
+**Required** Topic to create.
+
+## `REPLICATION_FACTOR`
+
+Kafka topic replication factor. Default `"2"`.
+
+## `NUM_PARTITIONS`
+
+Kafka topic number of partitions. Default `"3"`.
+
+## `CLEANUP_POLICY`
+
+Kafka topic cleanup policy. Default `"delete"`.
+
+## Outputs
+
+Nothing...
+
+## Example usage
+
+uses: actions/create-kafka-topic@v1.1
+with:
+  KAFKA_BROKER_URL: 'http://localhost:8080'
+  KAFKA_SSL_CA: 'begin_certificate...end_certificate'
+  KAFKA_SSL_KEY: 'begin_private_key...end_private_key'
+  KAFKA_SSL_CERT: 'begin_certificate...end_certificate'
+  TOPIC: 'my-topic.v1'
+  REPLICATION_FACTOR: '3'
+  NUM_PARTITIONS: '6'
+  CLEANUP_POLICY: 'compact'
+
+## Publish a new version
+
+Install vercel to compile project
+```
+    npm i -g @vercel/ncc
+```
+
+Build file with accompanying licences to the packages
+```
+    ncc build index.js --license licenses.txt
+```
+
+Check in changes
+```
+git commit . -m "commit message"
+git tag -a -m "My first action release" v1.1
+git push --follow-tags
+```
---
+++ index.js
@@ -0,0 +1,48 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const {Kafka} = require('kafkajs');
+const {SchemaRegistry, SchemaType} = require('@kafkajs/confluent-schema-registry');
+
+(async () => {
+    try {
+        const KAFKA_BROKER_URL = core.getInput("KAFKA_BROKER_URL")
+        const KAFKA_SSL_CA = core.getInput("KAFKA_SSL_CA")
+        const KAFKA_SSL_KEY = core.getInput("KAFKA_SSL_KEY")
+        const KAFKA_SSL_CERT = core.getInput("KAFKA_SSL_CERT")
+        const TOPIC = core.getInput("TOPIC")
+        const REPLICATION_FACTOR = core.getInput("REPLICATION_FACTOR")
+        const NUM_PARTITIONS = core.getInput("NUM_PARTITIONS")
+        const CLEANUP_POLICY = core.getInput("CLEANUP_POLICY")
+    
+        createTopic(
+            TOPIC,
+            kafkaClient(KAFKA_BROKER_URL, KAFKA_SSL_CA, KAFKA_SSL_KEY, KAFKA_SSL_CERT)
+        )
+    
+        const adminClient = new Kafka({
+            clientId: 'create-kafka-topic-gha',
+            brokers: [KAFKA_BROKER_URL],
+            ssl: {
+                ca: [KAFKA_SSL_CA],
+                key: KAFKA_SSL_KEY,
+                cert: KAFKA_SSL_CERT
+            }
+        }).admin()
+        
+        await adminClient.connect()
+        await adminClient.createTopics({
+            topics: [{
+                topic: topicName,
+                replicationFactor: REPLICATION_FACTOR,
+                numPartitions: NUM_PARTITIONS,
+                configEntries: [
+                    {name: "cleanup.policy", value: CLEANUP_POLICY},
+                ],
+            }],
+        })
+        await adminClient.disconnect()
+    } catch (e) {
+        core.setFailed(e.message)
+    }
+})()
+
